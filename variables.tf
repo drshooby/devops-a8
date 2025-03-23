978e127fd8530f04d9b48f2afb8143589976d7f0
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-03-23 13:50:46 -0700

    add bastion and variables

--- variables.tf
+++ variables.tf
@@ -33,3 +33,42 @@ variable "private_subnet_cidrs" {
   default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   description = "Private subnet CIDR blocks."
 }
+
+variable "public_key" {
+  type        = string
+  default     = "ssh-rsa xxxxxxxxx"
+  description = "Public key to be used for bastion host."
+}
+
+variable "bastion_prefix" {
+  type       = string
+  default    = "bastion-a8"
+  description = "Prefix for bastion host."
+}
+
+variable "resource_tags" {
+  type       = map(string)
+  default    = {
+    Terraform   = "true"
+    Environment = "local"
+  }
+  description = "A map of tags to add to all resources."
+}
+
+variable "ssh_key_name" {
+  type        = string
+  default     = "bastion-key"
+  description = "SSH key name."
+}
+
+variable "bastion_allowed_cidr" {
+  type        = list(string)
+  default     = ["xxx.xxx.xxx.xxx/32"]
+  description = "CIDR block to allow SSH access to bastion host."
+}
+
+variable "iam_instance_profile" {
+  type        = string
+  default     = "LabInstanceProfile"
+  description = "IAM instance profile name."
+}
