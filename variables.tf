a2fa0feb4c4728d793dc091334220df5326ec0c3
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-03-22 23:22:45 -0700

    follow Mario's example

--- variables.tf
+++ variables.tf
@@ -3,3 +3,33 @@ variable "aws_region" {
   default     = "us-east-1"
   description = "AWS Default region."
 }
+
+variable "vpc_name" {
+  type        = string
+  default     = "my-vpc"
+  description = "VPC name."
+}
+
+variable "vpc_cidr" {
+  type        = string
+  default     = "10.0.0.0/16"
+  description = "VPC CIDR block."
+}
+
+variable "aws_azs" {
+  description = "List of az in the specified region"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "public_subnet_cidrs" {
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  description = "Public subnet CIDR blocks."
+}
+
+variable "private_subnet_cidrs" {
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  description = "Private subnet CIDR blocks."
+}
